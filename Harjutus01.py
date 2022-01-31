5b14d26eb7e86041f703ecbf676ef03dac78d917
KasparIT21 <98739379+KasparIT21@users.noreply.github.com>
2022-01-31 11:31:36 +0200

    Add files via upload

---
+++ Harjutus01.py
@@ -0,0 +1,44 @@
+#Harjutus 01
+#Kaspar Tõnisson
+#31.01.2022
+
+print ("\"Cabernet Sauvignon\" või 'Cabernet Sauvignon'?")
+print ("  @..@")
+print (" (----)")
+print ("( \__/ )")
+print ("^^ \"\" ^^")
+
+a = int(input("Lisa ruudu külg:"))
+p = a * a
+print ("Vastus: Ruudu pindala on",p,"cm^2")
+
+
+
+
+
+
+'''
+print ("tere maailm") #sõne
+print ('tere sa ka') #string
+print (2000) #int / täisarv
+print (2.5) #float / komaarv
+print ("Ütle \"Tere maailm\"") #escape string
+print (type(2000)) #näitab andmetüüpi
+'''
+
+'''
+a = 12
+b = 16
+s = a*b
+p = 2*(a+b)
+print ("Pindala",s,"ruutmeetrit")
+print ("Ümbermõõt",p,"meetrit")
+'''
+
+'''
+nimi = input("Lisa oma nimi:")
+print ("Tere",nimi+"!") #Kui paneme lõppu + siis ei tule tühikut
+vanus = int(input("Lisa oma vanus:"))
+vanuste_vahe = 17-vanus
+print ("Meie vanuse vahe on",vanuste_vahe,"aastat")
+'''
